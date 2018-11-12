2689d6122de651ccab3cdcbad6021b19ed0e91fb
WaritsaraBambuttook <four_nukiz@hotmail.com>
2018-11-12 15:06:44 +0700

    4 table

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ var output = {
 app.get('/api/json',function(req,res){
     res.json(output);
 });
+// products
 //ดึง function มาจาก database.js เลย
 app.get('/api/products/', db.getAllProducts);
 //get id
@@ -32,6 +33,39 @@ app.post('/api/products', db.insertProduct);
 app.put('/api/products/:id' , db.updateProduct);
 //delete
 app.delete('/api/products/:id' , db.deleteProduct);
+
+//purchase_items
+app.get('/api/purchase_items/', db.getpurchase_items);
+//get id
+app.get('/api/purchase_items/:id' , db.getpurchase_itemsByID);
+//insert
+app.post('/api/purchase_items', db.insertpurchase_items);
+//update
+app.put('/api/purchase_items/:id' , db.updatepurchase_items);
+//delete
+app.delete('/api/purchase_items/:id' , db.deletepurchase_items);
+
+//purchase
+app.get('/api/purchase', db.getPurchase);
+//get id
+app.get('/api/purchase/:id', db.getPurchaseByID);
+//insert
+app.post('/api/purchase', db.insertPurchase);
+//update
+app.put('/api/purchase/:id', db.updatePurchase);
+//delete
+app.delete('/api/purchase/:id', db.DeletePurchase);
+
+//user
+app.get('/api/user', db.getUser);
+//get id
+app.get('/api/user/:id', db.getUserByID);
+//insert
+app.post('/api/user', db.insertUser);
+//update
+app.put('/api/user/:id', db.updateUser);
+//delete
+app.delete('/api/user/:id', db.DeleteUser);
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
