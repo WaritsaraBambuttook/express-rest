016a1926d5bef224ce9c1ca7e80afeea0260041e
waritsara <four_nukiz@hotmail.com>
2018-12-15 14:19:51 +0700

    chang db

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-var db = pgp('postgres://bjwszttquehmfk:a4f6bf708305c1f87a2caeb218400b856a0424698ac79e83c2e81bcc4c105a3e@ec2-184-73-169-151.compute-1.amazonaws.com:5432/d3otr9sl321kff?ssl=true');
+var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
 // Add queries here
 function getAllorderID(req, res) {
     db.any('select * from products')
