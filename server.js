c3f69d5e2243b84906d1c7af903a7c69b98792f7
waritsara <four_nukiz@hotmail.com>
2018-12-15 14:05:30 +0700

    channg db

--- server.js
+++ server.js
@@ -24,48 +24,8 @@ app.get('/api/json',function(req,res){
 });
 // products
 //ดึง function มาจาก database.js เลย
-app.get('/api/products/', db.getAllProducts);
-//get id
-app.get('/api/products/:id' , db.getProductByID);
-//insert
-app.post('/api/products', db.insertProduct);
-//update
-app.put('/api/products/:id' , db.updateProduct);
-//delete
-app.delete('/api/products/:id' , db.deleteProduct);
+app.get('/api/order/', db.getAllorderID);
 
-//purchase_items
-app.get('/api/purchase_items', db.getpurchase_items);
-//get id
-app.get('/api/purchase_items/:id' , db.getpurchase_itemsByID);
-//insert
-app.post('/api/purchase_items', db.insertpurchase_items);
-//update
-app.put('/api/purchase_items/:id' , db.updatepurchase_items);
-//delete
-app.delete('/api/purchase_items/:id' , db.deletepurchase_items);
-
-//purchase
-app.get('/api/purchase', db.getPurchase);
-//get id
-app.get('/api/purchase/:id', db.getPurchaseByID);
-//insert
-app.post('/api/purchase', db.insertPurchase);
-//update
-app.put('/api/purchase/:id', db.updatePurchase);
-//delete
-app.delete('/api/purchase/:id', db.DeletePurchase);
-
-//user
-app.get('/api/user', db.getUser);
-//get id
-app.get('/api/user/:id', db.getUserByID);
-//insert
-app.post('/api/user', db.insertUser);
-//update
-app.put('/api/user/:id', db.updateUser);
-//delete
-app.delete('/api/user/:id', db.DeleteUser);
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
